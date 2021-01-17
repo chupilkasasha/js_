b681e488b6ff15a5cd17779ceeae07bec4ce4b36
chupilkasasha <chupilkaa@gmail.com>
2021-01-17 20:08:14 +0200

    cycles

--- skript1.js
+++ skript1.js
@@ -8,11 +8,30 @@ const personalMovieDb = {
   privet: false
 }
 
-const a = prompt('Один из последних фильмов?');
-const b = prompt('На сколько оцените его?');
+
 const с = prompt('Один из последних фильмов?');
 const d = prompt('На сколько оцените его?');
 
-personalMovieDb.count[a] = b;
-personalMovieDb.count[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних фильмов?');
+  const b = prompt('На сколько оцените его?');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDb.count[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
 
+if (personalMovieDb.count < 10) {
+  console.log('просмотрено мало фильмов!');
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+  console.log('вы активный зритель фильмов!');
+} else if (personalMovieDb.count >= 30) {
+  console.log('вы киноман любитель!');
+} else {
+  console.log('произошла ошибка');
+}
+
+personalMovieDb.count[c] = d;
