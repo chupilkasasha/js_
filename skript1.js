6c67f0c935078e7c90b9e69f5c9c3704c9ad7924
chupilkasasha <chupilkaa@gmail.com>
2021-01-24 19:16:20 +0200

    add hw3

--- skript1.js
+++ skript1.js
@@ -1,5 +1,13 @@
-const numberOffFilms = +prompt('Сколько посмотрели фильмов?');
+let numberOffFilms;
+function start(){
+  numberOffFilms = +prompt('Сколько посмотрели фильмов?');
 
+  while(numberOffFilms == '' || numberOffFilms == null || numberOffFilms == isNaN(numberOffFilms)){
+    numberOffFilms = +prompt('Сколько посмотрели фильмов?');
+  }
+
+}
+start();
 const personalMovieDb = {
   count: numberOffFilms,
   movies: {},
@@ -8,30 +16,62 @@ const personalMovieDb = {
   privet: false
 }
 
-
 const с = prompt('Один из последних фильмов?');
 const d = prompt('На сколько оцените его?');
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних фильмов?');
-  const b = prompt('На сколько оцените его?');
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDb.count[a] = b;
-    console.log('done');
+function rememberFilms(){
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних фильмов?');
+    const b = prompt('На сколько оцените его?');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDb.count[a] = b;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  }
+}
+//rememberFilms();
+
+function detecPersonalLevel(){
+  if (personalMovieDb.count < 10) {
+    console.log('просмотрено мало фильмов!');
+  } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+    console.log('вы активный зритель фильмов!');
+  } else if (personalMovieDb.count >= 30) {
+    console.log('вы киноман любитель!');
   } else {
-    console.log('error');
-    i--;
+    console.log('произошла ошибка');
+  }
+}
+//detecPersonalLevel();
+
+
+function showMyDb(hidden){
+  if(!hidden){
+    console.log(personalMovieDb);
   }
 }
 
-if (personalMovieDb.count < 10) {
-  console.log('просмотрено мало фильмов!');
-} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
-  console.log('вы активный зритель фильмов!');
-} else if (personalMovieDb.count >= 30) {
-  console.log('вы киноман любитель!');
-} else {
-  console.log('произошла ошибка');
-}
+showMyDb(personalMovieDb.privet);
 
-personalMovieDb.count[c] = d;
+function writeYourGanres(){
+  for(let i = 1; i <= 3; i++){
+    const gener = prompt(`ваш любимый жанр под номером ${i}`);
+    personalMovieDb.genres[i-1] = gener;
+  }
+}
+writeYourGanres();
+
+
+
+
+
+
+
+
+
+
+
+
